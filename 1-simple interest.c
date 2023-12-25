54e632832a52b59b0658c21b3b14d1bbca57f012
Satyamzz <148209377+Satyamzz@users.noreply.github.com>
2023-12-25 23:07:26 +0530

    Update and rename 1-simple interest to 1-simple interest.c

--- 1-simple interest.c
+++ 1-simple interest.c
@@ -8,7 +8,6 @@ printf("enter the rate:");
 scanf(" %d",& rate);
 printf("enter the time:");
 scanf(" %d",& time);
-
 int simpleintrest=(pamount*rate*time)/100;
  printf("the simple intrest is %d\n",simpleintrest);
  printf("end amount is  %d",simpleintrest);
