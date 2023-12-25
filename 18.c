de42cfdeb0f2915183a35a24ea6f39ac3e4681fb
Satyamzz <148209377+Satyamzz@users.noreply.github.com>
2023-12-25 16:41:45 +0530

    Update 18.c

--- 18.c
+++ 18.c
@@ -1,42 +1,35 @@
 /*18. Multi-level Menu using Switch Case: Write a program to create a multi-level menu using
 switch cases that perform various basic mathematical operations (Add, Subtract, Divide,
-Multiply, Exponent).
-*/
-
+Multiply, Exponent).*/
 #include<stdio.h>
 int main()
 {
     int i=0;
-    int x,y,z;
+    int a,b,c;
     printf("1 for add,2 for subtract,3 for divison,4 for multiplication,5 for exponent");
     printf("\n");
-    printf("enter value of i,x and y:");
+    printf("enter value of i,a and b:");
     
-    scanf("%d%d%d",&i,&x,&y);
+    scanf("%d %d %d",&i,&a,&b);
     switch(i)
     {
         case 1:
-        z=x+y;
-        printf("additon= %d",z);
+       c=a+b;
+        printf("additon= %d",c);
         break;
         case 2:
-        z=x-y;
-        printf("subtraction=%d",z);
+      c=a-b;
+        printf("subtraction=%d",c);
         case 3:
-        z=x*y;
-        printf("multiplication= %d",z);
+     c=a*b;
+        printf("multiplication= %d",c);
         break;
         case 4:
-        z=x/y;
-        printf("division=%d",z);
+       c=a/b;
+        printf("division=%d",c);
         case 5:
-        z=x^y;
-        printf("exponential=%d",z);
-
-
-
-
+        c=a^b;
+        printf("exponential=%d",c);
     }
 return 0;
-
 }
